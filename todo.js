6f5ba28b3dd3bb1488d7cb8156da5fb9d79a4741
Rajesh Singh <rsingh2@walmartlabs.com>
2018-12-26 19:12:08 -0800

    lastest changes

--- todo.js
+++ todo.js
@@ -29,3 +29,33 @@ area-lable
 
 aria-label does much the same thing, but it's for those cases where it isn't practical or desirable to have a label on screen. Take the MDN example:
 
+
+
+
+CSS Grid vs Flexbox
+
+
+CSS Grid Layout is a two-dimensional system, meaning it can handle both columns and rows, unlike flexbox which is largely a one-dimensional system (either in a column or a row).
+A core difference between CSS Grid and Flexbox is that — CSS Grid’s approach is layout-first while Flexbox’ approach is content-first. 
+If you are well aware of you content 
+
+
+css property to add container to width
+
+
+CSS box-sizing Property
+
+#example1 {
+    box-sizing: border-box;
+  }
+
+Include padding and border in the element's total width and height:
+
+3. difference between CSS Grid And Flexbox
+4. CSS box-sizing Property (css property to add container to width)
+5. Properties for the Parent flex container/ flex items
+6. css property to add container to width
+7. Explain the concept of a component-based architecture, and how it relates to CSS as
+8. In React, what is the difference between state and props
+9. A 500 INTERNAL SERVER ERROR
+10. What is a scalable vector graphic?
