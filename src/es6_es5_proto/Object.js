10f852a7a99c86833a4da43e1fb7757d143866bd
Rajesh Singh <rsingh2@walmartlabs.com>
2019-01-16 03:33:08 -0800

    changes

---
+++ src/es6_es5_proto/Object.js
@@ -0,0 +1,103 @@
+/*
+Objects/ Clousure
+objects have a special hidden property [[Prototype]]
+t __proto__ is not the same as [[Prototype]]. 
+That’s a getter/setter for it.
+
+
+
+*/
+
+let animal = { 
+    eats: true
+};
+let rabbit = {
+    jumps: true 
+};
+rabbit.__proto__ = animal; //This line sets animal to be a prototype of rabbit.
+console.log(rabbit.eats)
+
+
+function Person(name){ 
+    this.name =name;
+}
+Person.prototype.syaName = function () { 
+    console.log(this.name);
+}
+var p1 = new Person("dd");
+p1.syaName();
+var p2 = new Person("mm");
+p2.syaName();
+
+
+
+function Rectangle (len,w){ 
+    this.len =len;
+    this.w = w;
+    this.getArea = function () {
+        console.log(this.len * this.w);
+    }
+}
+function Square(s){ 
+    this.len =s;
+    this.w = s;
+}
+Square.prototype = new Rectangle();
+Square.prototype.construcgor = Square;
+
+var sq = new Square(5);
+sq.getArea();
+
+
+//Object.create 
+
+prototypeObject = {
+	fullName: function(){
+		return this.firstName + " " + this.lastName		
+	}
+}
+
+var person = Object.create(prototypeObject, {
+      'firstName': {
+            value: "Virat", 
+            writable: true, 
+            enumerable: true
+      },
+      'lastName': {
+                value: "Kohli",
+                writable: true,
+                enumerable: true
+      }
+})
+    
+console.log(person) 
+
+
+
+// strict mode is important, as otherwise instead of a TypeError // you will just have a silent failure
+'use strict'
+const obj = { 
+    answer: 42
+} 
+Object.freeze(obj)
+obj.answer = 43 // throws TypeError about read only property obj.newProperty = 'foo' // throws TypeError about object not being extensible Object.defineProperty(obj, 'bar', {value: 'bar'}) // the same TypeError
+
+
+
+let parent = { 
+    type: "parent",
+    getName: function(){
+        return this.type;
+    }
+};
+let child = {
+    type: "child" 
+};
+
+
+
+child.__proto__ = parent; //This line sets animal to be a prototype of rabbit.
+console.log(child.getName());
+console.log(parent.getName());
+
+console.log(    parent.getName.call(child,null)     ); // IMP parent.getName no () just func name
---
+++ src/es6_es5_proto/modulepattern_closure.js
@@ -0,0 +1,125 @@
+/*
+Module Pattern
+Closure : closure are block that access the data outside of its scope
+*/
+
+/*
+1 Creating a module
+
+First we start using a anonymous closure.
+
+Anonymous closures are just functions that wrap our code and create an enclosed scope around it.
+Closures help keep any state or privacy within that function.
+Closures are one of the best and most powerful features of JavaScript.
+
+(
+    function() {
+    'use strict';
+    // Your code here
+    // All function and variables are scoped to this function
+    } 
+());
+
+This pattern is well known as a Immediately Invoked Function Expression or IIFE. 
+The function is evaluated then immediately invoked. Its also a good practice to run your modules in ES5 strict mode. 
+Strict mode will protect you from some of the more dangerous parts in JavaScript.
+
+*/
+
+/*
+
+2 Exporting our module
+Next we will want to export our module. 
+This basically assigns the module to a variable that we can use to call our modules methods.
+
+var myModule =
+(
+    function() { 
+    'use strict';
+    // Your code here
+    // All function and variables are scoped to this function 
+    }
+());
+
+*/
+
+/** 
+ 
+Either way is fine var x = ( function(){} () ); or
+Either way is fine var x = ( function(){} )();
+
+*/
+
+var module = (  
+    function() {
+        'use strict'
+
+        return {
+            publicMethod : function() {
+                console.log("Hellow World");
+            }
+        }
+    }()
+);
+
+module.publicMethod();
+
+/**
+ 
+4 Private methods & properties
+JavaScript does not have a private keyword by default but using closures 
+we can create private methods and private state.
+
+An important feature of closures is that an inner function 
+still has access to the outer function’s variables.
+
+ */
+
+
+var person = (
+    function() {
+        var age = 25;
+        return {
+            name : "raj",
+            getAge : function() {
+                return age;
+            },
+            growOlder : function() {
+                age++;
+            }
+
+        }
+    }
+());
+
+console.log(person.name); //raj
+console.log(person.getAge()); //25
+person.growOlder();
+console.log(person.getAge()); //26
+
+console.log(person.age);
+// module pattern using Constructor Function 
+
+function PersonF(name) {
+
+    var age = 25;
+    this.name = name;
+
+    this.getAge = function() {
+        return age;
+    };
+
+    this.growOlder = function() {
+        age++;
+    };
+
+}
+
+var p = new PersonF("raj");
+
+console.log(p.name); //raj
+console.log(p.getAge()); //25
+p.growOlder(); 
+console.log(p.getAge()); // 26
+
+console.log(p.age); //undefined
