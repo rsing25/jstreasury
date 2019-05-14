6be09b5252139c4e2ab179f4c027175561d4fe88
Rajesh Singh <rsingh2@walmartlabs.com>
2019-05-13 21:35:23 -0700

    .

---
+++ src/1basic.js/hadoop_hive.js
@@ -0,0 +1,12 @@
+Rundeck - to schedule spark jobs
+
+ Hive - Database on top of HDFS HDFS - 
+
+hadoop distributed file system 
+
+HUE - Query tool for HIVE 
+
+Pig - Like PLSQL in HIVE 
+
+
+===============================================================================================
