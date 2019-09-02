37a02ada9070dab9839cfe59d475ff23bdb823f3
Rajesh Singh <rsingh2@walmartlabs.com>
2019-09-02 03:30:37 -0700

    .

---
+++ practise.js
@@ -0,0 +1,19 @@
+var add = function(x) {
+
+    return function(y) {
+
+        return function(z) {
+            return x+y+z;
+        }
+    }
+
+}
+
+console.error(add(1)(2)(3));
+
+var add1 = function(a) {
+
+    console.log(add1.length);
+}
+
+console.error(add1.bind(this,1,2)());
